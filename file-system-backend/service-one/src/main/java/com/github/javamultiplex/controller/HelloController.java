d0e6eeb4a02c445800919b3577cfda2ff325b67b
Rohit Agarwal <programmingwithrohit@gmail.com>
2022-05-21 00:15:30 +0530

    Added spring cloud config for file system backend

---
+++ file-system-backend/service-one/src/main/java/com/github/javamultiplex/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.github.javamultiplex.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Rohit Agarwal on 20/05/22 11:47 PM
+ * @copyright github.com/javamultiplex
+ */
+@RestController
+public class HelloController {
+
+    @Value("${hello.message}")
+    public String message;
+
+    @GetMapping("/hello")
+    public String hello(){
+        return message;
+    }
+}
